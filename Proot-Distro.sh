b6cf0f2dc58842dfa9da0234553c95b5fe7d325e
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2022-11-25 15:34:46 +0700

    Create Proot-Distro.sh

---
+++ Proot-Distro.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+dpkg --add-architecture armhf
+
+apt update && apt upgrade -y
+
+apt-get install sudo wget nano inetutils-tools dialog -y
+
+apt-get install xfce4 xfce4-goodies xfce4-terminal exo-utils tigervnc-standalone-server tigervnc-common dbus-x11 --no-install-recommends -y
+
+apt-get clean
+
+mkdir -p ~/.vnc
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/xstartup -P ~/.vnc/
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/vncserver-start -P /usr/local/bin/
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/vncserver-stop -P /usr/local/bin/
+chmod +x /usr/local/bin/vncserver-start
+chmod +x /usr/local/bin/vncserver-stop
+chmod +x ~/.vnc/xstartup
+
+echo "export DISPLAY=":1"" >> /etc/profile
+source /etc/profile
+
+echo "vncserver -name remote-desktop -geometry 960x540 :1" > /usr/local/bin/vncserver-start
+
+echo -e "vncserver -kill :1\nrm -rf /root/.vnc/localhost:1.pid\nrm -rf /tmp/.X1-lock\nrm -rf /tmp/.X11-unix/X1" > ~/.bash_logout
+
+vncpasswd
+vncserver-start
