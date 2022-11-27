32aaf4e5515494c3a21a915576ece78534fa207c
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2022-11-27 16:22:07 +0700

    Update Fex-emu.sh

--- Fex-emu.sh
+++ Fex-emu.sh
@@ -1,3 +1,11 @@
+# Install Fex
 curl --silent https://raw.githubusercontent.com/FEX-Emu/FEX/main/Scripts/InstallFEX.py --output /tmp/InstallFEX.py && 
 python3 /tmp/InstallFEX.py && 
 rm /tmp/InstallFEX.py
+
+# Fix Chroot
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+# Fix Fex-emu
+sudo apt install curl software-properties-common
+
