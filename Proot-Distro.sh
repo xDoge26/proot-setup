cb1690f3e759d039b9ef5fb157e1122631dc6abd
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2022-12-28 23:00:44 +0700

    Update Proot-Distro.sh

--- Proot-Distro.sh
+++ Proot-Distro.sh
@@ -10,6 +10,7 @@ apt-get install xfce4 xfce4-terminal --no-install-recommends -y
 
 apt install adwaita-icon-theme-full -y
 apt install tango-icon-theme -y
+apt install gnome-themes-extra -y
 update-icon-caches /usr/share/icons/*
 
 apt clean && apt autoremove -y
