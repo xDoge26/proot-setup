c51f386fb039659aaf8f6bcbf03a4ce677e558e6
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2022-12-29 18:42:42 +0700

    Update Proot-Distro.sh

--- Proot-Distro.sh
+++ Proot-Distro.sh
@@ -14,8 +14,18 @@ update-icon-caches /usr/share/icons/*
 apt clean && apt autoremove -y
 
 mkdir ~/.vnc
-wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/xstartup -P ~/.vnc/
-wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/vncserver-start -P /usr/local/bin/
+# wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/xstartup -P ~/.vnc/
+
+echo "#!/bin/bash
+xrdb $HOME/.Xresources
+startxfce4" > ~/.vnc/xstarup
+
+
+# wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/vncserver-start -P /usr/local/bin/
+
+echo "vncserver -name remote-desktop -geometry 960x540 -localhost no :1" > /usr/local/bin/vncserver-start
+
+
 wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/vncserver-stop -P /usr/local/bin/
 chmod +x /usr/local/bin/vncserver-start
 chmod +x /usr/local/bin/vncserver-stop
