558361fb7094d9573bc4664d9bb1d4c8c19ff2c5
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-02-16 14:53:33 +0700

    Update stop.sh

--- Chroot/stop.sh
+++ Chroot/stop.sh
@@ -1,9 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
 umount ./chroot/dev/pts
-umount ./chroot/sdcard
 umount ./chroot/dev
-umount ./chroot/sys
 umount ./chroot/proc
-
+umount ./chroot/sys
+umount ./chroot/sdcard
 
