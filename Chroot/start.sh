71b18035d4a8c4df6a7c9d5aed22416bb4c28633
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-02-22 10:26:18 +0700

    Update start.sh

--- Chroot/start.sh
+++ Chroot/start.sh
@@ -17,3 +17,9 @@ export TERM=$TERM
 export TMPDIR=/tmp
 
 chroot ./chroot /bin/su - root
+
+umount umount -lvf ./chroot/dev/pts
+umount umount -lvf ./chroot/dev
+umount umount -lvf ./chroot/proc
+umount umount -lvf ./chroot/sys
+umount umount -lvf ./chroot/sdcard
