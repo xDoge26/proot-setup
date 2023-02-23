f3694726c4bf19d697c1547c34a4f8afc7e6e2ee
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-02-23 14:47:51 +0700

    Update Proot-Distro.sh

--- Proot-Distro.sh
+++ Proot-Distro.sh
@@ -2,7 +2,7 @@
 
 apt update && apt upgrade -y
 
-apt-get install sudo nano wget xfe dbus-x11 tigervnc-standalone-server -y
+apt-get install sudo nano wget tzdata dbus-x11 tigervnc-standalone-server -y # xfe
 apt-get install xfce4 xfce4-terminal --no-install-recommends -y
 
 apt install adwaita-icon-theme-full -y
