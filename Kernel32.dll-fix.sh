e1f13a8c9b00acf407a43a75f4d9d603e78d2104
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-02-25 10:59:13 +0700

    Update Kernel32.dll-fix.sh

--- Kernel32.dll-fix.sh
+++ Kernel32.dll-fix.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+rm -rf ~/.wine
+wget https://github.com/ThieuMinh26/Proot-Setup/releases/download/1.0/64bit-wine.tar.xz
+echo 'alias gts=WINEDLLOVERRIDES="winegstreamer=" >> ~/.bashrc
+source ~/.bashrc
+clear
+
+echo If you have kernel32.dll problem , restart Termux
+echo And please use : gst box86 wine winecfg 
+echo Or : rm -rf ~/.wine && WINEARCH=win32 box86 wine winecfg
