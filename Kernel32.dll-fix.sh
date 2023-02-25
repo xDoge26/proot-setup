e9eaf6da0c76c67f867a672150c9c7ef648490b5
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-02-25 11:07:13 +0700

    Update Kernel32.dll-fix.sh

--- Kernel32.dll-fix.sh
+++ Kernel32.dll-fix.sh
@@ -2,7 +2,7 @@
 
 rm -rf ~/.wine
 wget https://github.com/ThieuMinh26/Proot-Setup/releases/download/1.0/64bit-wine.tar.xz
-echo 'alias gts=WINEDLLOVERRIDES="winegstreamer=" >> ~/.bashrc
+echo 'alias gts=WINEDLLOVERRIDES="winegstreamer="' >> ~/.bashrc
 source ~/.bashrc
 clear
 
