b27a0baf79ecef6d529b92f4b5bbcd28a49d7853
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-02-25 23:34:30 +0700

    Update Proot-Distro.sh

--- Proot-Distro.sh
+++ Proot-Distro.sh
@@ -5,7 +5,7 @@ apt update && apt upgrade -y
 apt-get install sudo nano wget tzdata dbus-x11 tigervnc-standalone-server -y 
 apt-get install xfce4 xfce4-terminal --no-install-recommends -y
 
-# apt install adwaita-icon-theme-full -y
+apt install adwaita-icon-theme-full -y
 apt install gnome-themes-extra -y
 apt install tango-icon-theme -y
 # update-icon-caches /usr/share/icons/*
