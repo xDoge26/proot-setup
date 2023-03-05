bd8e15441dc3e4ed13e4f2d9b99f90d00f0ea13d
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-03-05 20:40:47 +0700

    Create busybox-start.sh

---
+++ Chroot/busybox-start.sh
@@ -0,0 +1,19 @@
+#!/system/bin/sh
+
+mount --bind /proc ./chroot/proc
+mount --bind /sys ./chroot/sys
+mount --bind /dev ./chroot/dev
+mount --bind /dev/pts ./chroot/dev/pts
+mount --bind /sdcard ./chroot/sdcard
+# mount --bind /data/data/com.termux/files/usr/tmp ./chroot/tmp
+# mount --bind /data/data/com.mittorn.virgloverlay/files/ ./chroot/tmp
+
+
+chroot ./chroot /bin/su - root
+
+umount -lv ./chroot/dev/pts
+umount -lv ./chroot/dev
+umount -lv ./chroot/sys
+umount -lv ./chroot/proc
+umount -lv ./chroot/sdcard
+# umount -lv ./chroot/tmp
