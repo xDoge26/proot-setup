c840868cc43060518b6147674821281670163de3
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-03-07 18:07:58 +0700

    Update busybox-start.sh

--- Chroot/busybox-start.sh
+++ Chroot/busybox-start.sh
@@ -5,8 +5,7 @@ mount --bind /sys ./chroot/sys
 mount --bind /dev ./chroot/dev
 mount --bind /dev/pts ./chroot/dev/pts
 mount --bind /sdcard ./chroot/sdcard
-# mount --bind /data/data/com.termux/files/usr/tmp ./chroot/tmp
-# mount --bind /data/data/com.mittorn.virgloverlay/files/ ./chroot/tmp
+mount --bind /data/data/com.termux/files/usr/tmp ./chroot/tmp
 
 chroot ./chroot /bin/su - root
 
@@ -15,4 +14,4 @@ umount -lv ./chroot/dev
 umount -lv ./chroot/sys
 umount -lv ./chroot/proc
 umount -lv ./chroot/sdcard
-# umount -lv ./chroot/tmp
+umount -lv ./chroot/tmp
