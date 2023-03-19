c7a3dfc5be81ad240669c0805094982dd19fd733
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-03-19 20:27:15 +0700

    Delete stop.sh

--- Chroot/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/sh
-
-umount -lv ./chroot/dev/pts
-umount -lv ./chroot/dev
-umount -lv ./chroot/sys
-umount -lv ./chroot/proc
-umount -lv ./chroot/sdcard
-# umount -lv ./chroot/tmp
-
