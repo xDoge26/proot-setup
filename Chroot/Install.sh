bd16f80e34be60ece3820b12afb2a29145f4fe46
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-04-03 19:17:40 +0700

    Update Install.sh

--- Chroot/Install.sh
+++ Chroot/Install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+UBUNTU="/data/chroot"
+BUSYBOX="/data/adb/magisk/busybox"
+
 mkdir /data/chroot
+mkdir /data/chroot/sdcard
 cd /data/chroot
-wget http://cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/ubuntu-base-22.04.2-base-arm64.tar.gz 
-tar -xvpf *.tar.gz && sudo rm *.tar.gz && clear &&
-mkdir /data/chroot/sdcard 
+$BUSYBOX wget http://cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/ubuntu-base-22.04.2-base-arm64.tar.gz 
+$BUSYBOX tar -xvpf *.tar.gz 
+rm *.tar.gz 
+ 
