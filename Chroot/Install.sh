9a291df87e0114e6db9f48214fdd6ade9a066338
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-04-03 19:00:06 +0700

    Update Install.sh

--- Chroot/Install.sh
+++ Chroot/Install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-su -c mkdir /data/chroot
-su -c cd /data/chroot
+mkdir /data/chroot
+cd /data/chroot
 wget http://cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/ubuntu-base-22.04.2-base-arm64.tar.gz 
 tar -xvpf *.tar.gz && sudo rm *.tar.gz && clear &&
 mkdir /data/chroot/sdcard 
