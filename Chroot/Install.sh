7f4839eaa80b49c8053616239adbff5f4a13379c
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-04-03 18:55:43 +0700

    Update Install.sh

--- Chroot/Install.sh
+++ Chroot/Install.sh
@@ -1,14 +1,7 @@
-#!/data/data/com.termux/files/usr/bin/sh
+#!/bin/sh
 
-echo "allow-external-apps = true" >> ~/.termux/termux.properties 
-echo "hide-soft-keyboard-on-startup = true" >> ~/.termux/termux.properties
-
-pkg clean && termux-setup-storage && yes | pkg update &&
-pkg install tsu nano wget pulseaudio -y && pkg clean &&
-echo 'alias start="su -c ./busybox-start.sh"' >> ~/.bashrc &&
-echo 'alias stop="su -c ./busybox-stop.sh"' >> ~/.bashrc &&
-echo 'pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1' >> ~/.bashrc &&
-echo 'alias gl="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 virgl_test_server_android &"' >> ~/.bashrc &&
-echo 'alias zink="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy virgl_test_server --use-egl-surfaceless --use-gles &"' >> ~/.bashrc &&
-source ~/.bashrc
-
+sudo mkdir chroot
+cd chroot 
+sudo wget http://cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/ubuntu-base-22.04.2-base-arm64.tar.gz 
+sudo tar -xvpf *.tar.gz && sudo rm *.tar.gz && clear &&
+sudo mkdir sdcard 
