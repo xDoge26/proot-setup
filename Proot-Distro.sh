bcc9c0e4bd837f99ec14095adc340323f52da30d
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-04-10 15:54:17 +0700

    Update Proot-Distro.sh

--- Proot-Distro.sh
+++ Proot-Distro.sh
@@ -27,8 +27,8 @@ chmod +x ~/.vnc/xstartup
 chmod +x /usr/local/bin/vnc-start
 chmod +x /usr/local/bin/vnc-stop
 
-# echo "export DISPLAY=:1" >> /etc/profile
-# echo "export PULSE_SERVER=127.0.0.1" >> /etc/profile
-# source /etc/profile
+# echo "export DISPLAY=:1" >> ~/.bash_profile
+# echo "export PULSE_SERVER=127.0.0.1" >> ~/.bash_profile
+# source ~/.bash_profile
 
 
