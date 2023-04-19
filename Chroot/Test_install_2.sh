3a92fcec9a7ac267d6a913e04d9d060acabe47e2
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-04-19 19:09:04 +0700

    Update Test_install_2.sh

--- Chroot/Test_install_2.sh
+++ Chroot/Test_install_2.sh
@@ -12,4 +12,5 @@ pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth
 alias gl="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 virgl_test_server_android &"
 alias zink="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy virgl_test_server --use-egl-surfaceless --use-gles &"' > ~/.bashrc 
 
-source ~/.bashrc
+source ~/.bashrc 
+source ~/.termux/termux.properties
