95bc2e189f09157a998a1ce1b590838b74c87483
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-04-20 09:07:42 +0700

    Update Test_install.sh

--- Chroot/Test_install.sh
+++ Chroot/Test_install.sh
@@ -48,8 +48,11 @@ usermod -g 3003 _apt
 
 chmod 777 /tmp
 echo "vncserver -kill :1" > ~/.bash_logout
-echo "alias gl=\"MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT GALLIUM_DRIVER=virpipe WINEDEBUG=-all\"" >> ~/.bashrc
-echo "alias zink=\"MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT GALLIUM_DRIVER=virpipe WINEDEBUG=-all\"" >> ~/.bashrc
+echo "alias gl=\"MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT GALLIUM_DRIVER=virpipe WINEDEBUG=-all\"
+alias zink=\"MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT GALLIUM_DRIVER=virpipe WINEDEBUG=-all\"
+alias fexbash=\"FEXBash\"
+alias fexcfg="\FEXConfig\"
+alias fex="\FEXInterpreter\"" >> ~/.bashrc
 source ~/.bashrc' > ~/test.sh
 
 echo '#!/bin/sh
