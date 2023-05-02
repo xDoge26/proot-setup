e61d3d4338c4d651e74099ad601d7034da36db34
Jank9525 <92904701+ThieuMinh26@users.noreply.github.com>
2023-05-02 20:50:30 +0700

    Update Box86-64_Wine86-64.sh

--- Box86-64_Wine86-64.sh
+++ Box86-64_Wine86-64.sh
@@ -25,18 +25,20 @@ sudo apt clean
 sudo apt autoremove -y
 
 # Install Box86
-wget --continue $BOX86_DEB && dpkg -i box86*.deb 
-rm --force box86*.deb
+# wget --continue $BOX86_DEB && dpkg -i box86*.deb 
+# rm --force box86*.deb
 
-# wget https://ryanfortner.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list
-# wget -qO- https://ryanfortner.github.io/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg 
+wget https://ryanfortner.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list
+wget -qO- https://ryanfortner.github.io/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg 
 
 # Install Box64
-wget --continue $BOX64_DEB && dpkg -i box64*.deb 
-rm --force box64*.deb
+# wget --continue $BOX64_DEB && dpkg -i box64*.deb 
+# rm --force box64*.deb
 
-# wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
-# wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg 
+wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
+wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg 
+
+sudo apt install box86-android box64-android
 
 # Wine-amd64
 rm -rf ~/wine
