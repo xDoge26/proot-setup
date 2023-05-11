28dfff508417e5dd222ece8dd1d1d5ef393f07b3
Jank9525 <92904701+xDoge26@users.noreply.github.com>
2023-05-11 13:02:27 +0700

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -3,7 +3,7 @@
 apt update && apt upgrade -y
 
 apt install -y sudo nano wget tzdata dbus-x11 tigervnc-standalone-server adwaita-icon-theme-full gnome-themes-extra
-apt install -y xfce4 xfce4-terminal --no-install-recommends
+apt install -y xfce4 xfce4-terminal --no-install-recommends --no-install-suggests
 
 # tango-icon-theme
 
