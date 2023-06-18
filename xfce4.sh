2b457ba77edcf9ba7a58724d562c04c8e4ad5f88
Jank9525 <92904701+xDoge26@users.noreply.github.com>
2023-06-18 14:57:06 +0700

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -14,12 +14,12 @@ export PULSE_SERVER=127.0.0.1
 xrdb $HOME/.Xresources
 taskset -c 4-7 startxfce4' > ~/.vnc/xstartup
 
+echo 'taskset -c 4-7 termux-x11 -xstartup startxfce4 &' > /usr/local/bin/x11
 echo 'vncserver -name remote-desktop -geometry 960x540 -localhost no :1' > /usr/local/bin/vnc-start
 echo 'vncserver -kill :1' > /usr/local/bin/vnc-stop
-echo 'taskset -c 4-7 termux-x11 -xstartup startxfce4 &' > /usr/local/bin/x11
 
 chmod +x ~/.vnc/xstartup
+chmod +x /usr/local/bin/x11
 chmod +x /usr/local/bin/vnc-start
 chmod +x /usr/local/bin/vnc-stop
-chmod +x /usr/local/bin/x11
-# tango-icon-theme
+
