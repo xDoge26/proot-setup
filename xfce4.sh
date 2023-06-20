52960a4b89cf73fe8cc8595d1917edd7c740cfac
Jank9525 <92904701+xDoge26@users.noreply.github.com>
2023-06-20 14:31:17 +0700

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -15,8 +15,8 @@ xrdb $HOME/.Xresources
 taskset -c 4-7 startxfce4' > ~/.vnc/xstartup
 
 echo 'taskset -c 4-7 termux-x11 :1 -xstartup startxfce4 &' > /usr/local/bin/x11
-echo 'vncserver -name remote-desktop -geometry 960x540 -localhost no :1' > /usr/local/bin/vnc-start
-echo 'vncserver -kill :1' > /usr/local/bin/vnc-stop
+echo 'vncserver :1 -name remote-desktop -geometry 960x540 -localhost no' > /usr/local/bin/vnc-start
+echo 'vncserver -kill' > /usr/local/bin/vnc-stop
 
 chmod +x ~/.vnc/xstartup
 chmod +x /usr/local/bin/x11
