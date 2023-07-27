3866559b5a0143f150e7ad8219a77ad7fc226392
Jank9525 <92904701+xDoge26@users.noreply.github.com>
2023-07-27 16:32:04 +0700

    Update Test_install.sh

--- Chroot/Test_install.sh
+++ Chroot/Test_install.sh
@@ -43,9 +43,7 @@ usermod -a -G 3003 root
 usermod -a -G 3003 _apt
 usermod -g 3003 _apt
 
-echo "alias gl=\"MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_EXTENSION_OVERRIDE=\"GL_EXT_polygon_offset_clamp\" GALLIUM_DRIVER=virpipe WINEDEBUG=-all\"
-alias zink=\"MESA_LOADER_DRIVER_OVERRIDE=zink TU_DEBUG=noconform MESA_VK_WSI_DEBUG=sw\"
-alias vk=\"TU_DEBUG=noconform MESA_VK_WSI_DEBUG=sw\"
+echo "
 alias fexbash=\"FEXBash\"
 alias fexcfg=\"FEXConfig\"
 alias fex=\"FEXInterpreter\"" >> ~/.bashrc
