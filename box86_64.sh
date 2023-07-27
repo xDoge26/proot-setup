b189ac2e163597298a927e97e18c9b003afcf092
Jank9525 <92904701+xDoge26@users.noreply.github.com>
2023-07-27 16:28:20 +0700

    Update box86_64.sh

--- box86_64.sh
+++ box86_64.sh
@@ -44,21 +44,34 @@ sudo ln -s ~/wine/bin/wine64 /usr/local/bin/wine64
 sudo chmod +x /usr/local/bin/wine /usr/local/bin/wine64
 
 # Setup something
-echo '#!/bin/bash
-DISPLAY=:1 \
-WINE_DEBUG=-all \
-TU_DEBUG=noconform \
-MESA_VK_WSI_DEBUG=sw \
-exec taskset -c 4-7 box86 wine "$@"
-' > /usr/local/bin/vulkan
 
 echo '#!/bin/bash
 DISPLAY=:1 \
 WINE_DEBUG=-all \
+MESA_NO_ERROR=1 \
+GALLIUM_DRIVER=virpipe \
+MESA_GL_VERSION_OVERRIDE=4.3COMPAT \
+MESA_EXTENSION_OVERRIDE="GL_EXT_polygon_offset_clamp" \
+exec taskset -c 4-7 box86 wine "$@"
+' > /usr/local/bin/gl
+
+echo '#!/bin/bash
+DISPLAY=:1 \
+WINE_DEBUG=-all \
+MESA_NO_ERROR=1 \
 TU_DEBUG=noconform \
 MESA_VK_WSI_DEBUG=sw \
 MESA_LOADER_DRIVER_OVERRIDE=zink \
 exec taskset -c 4-7 box86 wine "$@"
 ' > /usr/local/bin/zink
 
-sudo chmod +x /usr/local/bin/vulkan /usr/local/bin/zink
+echo '#!/bin/bash
+DISPLAY=:1 \
+WINE_DEBUG=-all \
+MESA_NO_ERROR=1 \
+TU_DEBUG=noconform \
+MESA_VK_WSI_DEBUG=sw \
+exec taskset -c 4-7 box86 wine "$@"
+' > /usr/local/bin/vulkan
+
+sudo chmod +x /usr/local/bin/vulkan /usr/local/bin/zink /usr/local/bin/gl
